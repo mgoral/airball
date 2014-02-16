536f7ea699c57826187ee086e4b62f8a893ba1c4
Michał Góral <virgoerns@gmail.com>
2014-02-16 22:18:32 +0100

    Replace Autotools with CMake.

---
+++ Makefile
@@ -0,0 +1,35 @@
+PROJECT_NAME=airball
+
+RM = rm -f
+MKDIR = mkdir -p
+CD = cd
+MAKE = make --no-print-directory
+CMAKE = cmake
+
+CMAKE_BUILD_DIR=./cmake
+
+.DEFAULT_GOAL = all
+
+.PHONY: prepare
+prepare:
+	$(MKDIR) $(CMAKE_BUILD_DIR)
+	$(CD) $(CMAKE_BUILD_DIR) && $(CMAKE) ..
+
+.PHONY: all
+all: prepare
+	$(CD) $(CMAKE_BUILD_DIR) && $(MAKE)
+
+# check is just alias
+check: ut
+
+ut: prepare
+	$(CD) $(CMAKE_BUILD_DIR) && $(MAKE) $(PROJECT_NAME)_ut_run
+
+.PHONY: clean
+clean:
+	$(CD) $(CMAKE_BUILD_DIR) && $(MAKE) clean
+
+.PHONY: distclean
+distclean:
+	$(RM) -r $(CMAKE_BUILD_DIR)
+
--- autogen.sh
+++ autogen.sh
@@ -1,28 +1,14 @@
 #!/bin/sh
 
+stepError() {
+    echo "--------------------------"
+    echo "An error occured during step $1"
+    echo "Exiting"
+    echo "--------------------------"
+    exit 1
+}
+
 echo "Preparing Airball build system..."
 
-autoreconf --force --install
-autorecst=$?
-
-if [ $autorecst -ne 0 ]; then
-    echo ""
-    echo "*** An ERROR occured on autoreconf! Check if there's a UNIX guru nearby to help you. ***"
-    exit 1
-fi
-
-#
-# Uncommented until there's no need for gettext
-#
-
-#intltoolize --force
-#intltoolst=$?
-#if [ $intltoolst -ne 0 ]; then
-#    echo ""
-#    echo "*** An ERROR occured on intltoolize! Is it installed on your system? ***"
-#    exit 1
-#fi
-
-echo ""
-echo "*** Build system ready. ***"
-echo ""
+echo "Preparing necessary git submodules..."
+git submodule update --init || stepError "submodules"
